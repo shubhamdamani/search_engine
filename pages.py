e4b542b076b549e43ec421c383e860f724e1f9a6
Shubham Damani <43433565+shubhamdamani@users.noreply.github.com>
2020-06-12 15:35:30 +0530

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 
 
@@ -7,18 +6,19 @@ conn = sqlite3.connect('spider.sqlite')
 cur = conn.cursor()
 
 cur.execute('SELECT html,id FROM Pages WHERE html is NOT NULL and error is NULL')
-for i in range(0,20):
-	row = cur.fetchone()[0]
-  name= cur.fetchone()[1]
-  name=name+".txt"
-  f=open(name,'w')
+for i in range(0,5):
+	row = cur.fetchone()#[0]
+	#print(row[1])
+	name = str(row[1])
+	name=name+".txt"
+	f=open(name,'w')
+ #  	name=name+".txt"
+ #  	f=open(name,'w')
 
-	#print(row[0])
+	# # #print(row[0])
 	if( type(row[0]) == type("str") ):
-		s=row[0]
-		u = unicode(s, "utf-8")
-		f.write(u)
+		
+		f.write(row[0])
 		f.write("\n\n\n\n")
 	#else:
 	#	f.write(row[0].decode("utf-8"))
-
